5798c6601efbeef740119a11ff3c33946ffcc2ca
Gleb <asd>
2022-04-23 19:07:50 +0600

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import {Toy} from './entities/Toy';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HttpService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'toy_shop';
+
+  toys: Toy[]=[];
+  bought_toys: string[]=[];
+     
+  constructor(private httpService: HttpService){}
+      
+  ngOnInit(){
+    this.httpService.getData().subscribe((data: any) => this.toys=data.toyList);
+  }
+
+  
+
+  public addToCart (_name: any){
+    this.bought_toys.push(_name);
+    console.log(this.bought_toys);
+  }
 }
---
+++ src/app/entities/Toy.ts
@@ -0,0 +1,3 @@
+export class Toy{
+    constructor(public title: string, public description: string, public cost: string, public photo?: string){}
+}
